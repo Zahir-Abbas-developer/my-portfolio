6c046587c114f9fe7d1a3827d1a7b40b21ab0146
zahir <zahirr060@gmail.com>
2023-12-15 06:30:02 -0800

    feat(): updated code

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Front End Software Developer 🚀 having an experience of building different Type Website  with JavaScript / Reactjs / Nextjs  and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/14d8HrTcubI9CPjMfw87BxYgy6HO16iDb/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1DM6zr-NvT28YBYLTw5NMNm3AboGfR8-G/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
